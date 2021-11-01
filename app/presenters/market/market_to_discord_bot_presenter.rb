4103c3713e0de4308798642c821c250c43ffcef7
Wellington Matheus <w.matheus@outlook.com>
2021-11-01 11:09:58 -0300

    refactor(MarketPresenter): create and use Market repository

--- app/presenters/market/market_to_discord_bot_presenter.rb
+++ app/presenters/market/market_to_discord_bot_presenter.rb
@@ -1,5 +1,15 @@
 class Market::MarketToDiscordBotPresenter
-  def self.build_message(market:)
+  def self.build
+    new(market_repository: Market)
+  end
+
+  def initialize(market_repository:)
+    @market_repository = market_repository
+  end
+
+  def build_message(market_id:)
+    market = @market_repository.find(market_id)
+
     market.title
   end
 end
