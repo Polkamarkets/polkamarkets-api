9a03f59171654bba58b0a8dd78d5bb75fae48633
Wellington Matheus <w.matheus@outlook.com>
2021-11-02 10:19:29 -0300

    feat(PublishMarketCreatedWorker): use send message to channel method

--- app/workers/discord/publish_market_created_worker.rb
+++ app/workers/discord/publish_market_created_worker.rb
@@ -2,7 +2,8 @@ class Discord::PublishMarketCreatedWorker
   include Sidekiq::Worker
 
   def perform(market_id)
-    message = Market::MarketToDiscordBotPresenter.build.build_message(market_id: market_id)
     bot = Discord::Bot.build.start
+    message = Market::MarketToDiscordBotPresenter.build.build_message(market_id: market_id)
+    bot.send_message_to_channel(channel_id: Config.discord.channel_id, message: message)
   end
 end
