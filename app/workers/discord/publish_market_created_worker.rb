c6aa6e6c4b42ceef2018357ff9ff1c1dd8cd060b
Wellington Matheus <w.matheus@outlook.com>
2021-11-02 23:19:16 -0300

    refactor(PublishMarketCreatedWorker): start bot after send message

--- app/workers/discord/publish_market_created_worker.rb
+++ app/workers/discord/publish_market_created_worker.rb
@@ -2,8 +2,9 @@ class Discord::PublishMarketCreatedWorker
   include Sidekiq::Worker
 
   def perform(market_id)
-    bot = Discord::Bot.build.start
+    bot = Discord::Bot.build
     message = Market::MarketToDiscordBotPresenter.build.build_message(market_id: market_id)
     bot.send_message_to_channel(channel_id: Config.discord.channel_id, message: message)
+    bot.start
   end
 end
