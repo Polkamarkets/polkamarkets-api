6d158e7bdf602d6c872342e8280f2d6a8ba442db
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-02-08 16:52:13 +0800

    add stats_networks

--- app/services/bepro/realitio_erc20_contract_service.rb
+++ app/services/bepro/realitio_erc20_contract_service.rb
@@ -2,11 +2,11 @@ module Bepro
   class RealitioErc20ContractService < SmartContractService
     include BigNumberHelper
 
-    def initialize(network_id:, url: nil, contract_address: nil)
+    def initialize(network_id: nil, api_url: nil, contract_address: nil)
       super(
         contract_name: 'realitio',
-        contract_address: Rails.application.config_for(:ethereum)["network_#{network_id}"]['realitio_contract_address'],
-        api_url: Rails.application.config_for(:ethereum)["network_#{network_id}"]['bepro_api_url']
+        contract_address: contract_address || Rails.application.config_for(:ethereum)["network_#{network_id}"]['realitio_contract_address'],
+        api_url: api_url || Rails.application.config_for(:ethereum)["network_#{network_id}"]['bepro_api_url']
       )
     end
 
--- app/services/stats_service.rb
+++ app/services/stats_service.rb
@@ -1,31 +1,46 @@
 class StatsService
-  attr_accessor :actions, :bonds
+  attr_accessor :networks
 
   def initialize
-    @actions = network_ids.map do |network_id|
-      [
-        network_id,
-        Bepro::PredictionMarketContractService.new(network_id: network_id).get_action_events
-      ]
-    end.to_h
+    # ethereum networks - markets are monitored within the api
+    ethereum_networks = network_ids.map do |network_id|
+      {
+        network_id: network_id,
+        bepro_pm: Bepro::PredictionMarketContractService.new(network_id: network_id),
+        bepro_realitio: Bepro::RealitioErc20ContractService.new(network_id: network_id)
+      }
+    end
 
-    @bonds = network_ids.map do |network_id|
-      [
-        network_id,
-        Bepro::RealitioErc20ContractService.new(network_id: network_id).get_bond_events
-      ]
-    end.to_h
+    # stats networks - only for stats, markets are not monitored within the api
+    stats_networks = stats_network_ids.map do |network_id|
+      {
+        network_id: network_id,
+        bepro_pm: Bepro::PredictionMarketContractService.new(
+          contract_address: Rails.application.config_for(:ethereum)["stats_network_#{network_id}"]['prediction_market_contract_address'],
+          api_url: Rails.application.config_for(:ethereum)["stats_network_#{network_id}"]['bepro_api_url']
+        ),
+        bepro_realitio: Bepro::RealitioErc20ContractService.new(
+          contract_address: Rails.application.config_for(:ethereum)["stats_network_#{network_id}"]['realitio_contract_address'],
+          api_url: Rails.application.config_for(:ethereum)["stats_network_#{network_id}"]['bepro_api_url']
+        )
+      }
+    end
+
+    @networks = ethereum_networks + stats_networks
   end
 
   def get_stats
     # TODO: volume chart
     # TODO: TVL chart
-    network_ids.map do |network_id|
+    networks.map do |network|
+      network_id = network[:network_id]
+      actions = network[:bepro_pm].get_action_events
+      bonds = network[:bepro_realitio].get_bond_events
 
-      volume = actions[network_id].select { |v| ['buy', 'sell'].include?(v[:action]) }
-      bonds_volume = bonds[network_id].sum { |bond| bond[:value] }
+      volume = actions.select { |v| ['buy', 'sell'].include?(v[:action]) }
+      bonds_volume = bonds.sum { |bond| bond[:value] }
       volume_movr = volume.sum { |v| v[:value] }
-      fee = Bepro::PredictionMarketContractService.new(network_id: network_id).get_fee
+      fee = network[:bepro_pm].get_fee
       fees_movr = volume.sum { |v| v[:value] } * fee
 
       [
@@ -62,5 +77,9 @@ class StatsService
 
   def network_ids
     @_network_ids ||= Rails.application.config_for(:ethereum).network_ids
+  end
+
+  def stats_network_ids
+    @_stats_network_ids ||= Rails.application.config_for(:ethereum).stats_network_ids
   end
 end
