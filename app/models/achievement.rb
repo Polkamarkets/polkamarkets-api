631ca72f8f29397cbb8be562f1eb2fa3de7fff5e
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-03-01 10:05:51 +0800

    Achievement: create from eth id

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,6 +2,8 @@ class Achievement < ApplicationRecord
   validates_presence_of :eth_id, :network_id, :action, :occurrences
   validates_uniqueness_of :eth_id, scope: :network_id
 
+  DEFAULT_IMAGE_URL = 'https://ipfs.infura.io:5001/api/v0/cat?arg=QmTxWoieQryavmHnYkgpiZTFLAwrovzmFjJfGUHvZX2JnP'
+
   enum action: {
     buy: 0,
     add_liquidity: 1,
@@ -10,22 +12,39 @@ class Achievement < ApplicationRecord
     create_market: 4,
   }
 
-  def action_events(refresh: false)
+  def self.create_from_eth_id!(network_id, eth_id)
+    raise "Achievement #{eth_id} is already created" if Achievement.where(network_id: network_id, eth_id: eth_id).exists?
+
+    achievement_ids = Bepro::AchievementsContractService.new(network_id: network_id).get_achievement_ids
+    # invalid achievement
+    raise "Achievement #{eth_id} does not exist" unless achievement_ids.include?(eth_id)
+
+    eth_data =
+      Rails.cache.fetch("achievements:network_#{network_id}:#{eth_id}", expires_in: 24.hours, force: true) do
+        Bepro::AchievementsContractService.new(network_id: network_id).get_achievement(eth_id)
+      end
+
+    achievement = Achievement.new(
+      action: eth_data[:action],
+      occurrences: eth_data[:occurrences],
+      eth_id: eth_id,
+      network_id: network_id
+    )
+
+    achievement.save!
+    achievement
+  end
+
+  def image_url
+    self['image_url'] || DEFAULT_IMAGE_URL
+  end
+
+  def eth_data(refresh: false)
     return @eth_data if @eth_data.present? && !refresh
 
     @eth_data ||=
-      Rails.cache.fetch("achievements:network_#{network_id}:data", force: refresh) do
-        Bepro::AchievementsContractService.new(network_id: network_id).get_action_events(address: eth_address)
+      Rails.cache.fetch("achievements:network_#{network_id}:#{eth_id}", force: refresh) do
+        Bepro::AchievementsContractService.new(network_id: network_id).get_achievement(eth_id)
       end
-  end
-
-  def refresh_cache!
-    # disabling cache delete for now
-    # $redis_store.keys("portfolios:#{eth_address}*").each { |key| $redis_store.del key }
-
-    # triggering a refresh for all cached ethereum data
-    Cache::PortfolioActionEventsWorker.perform_async(id)
-    Cache::PortfolioHoldingsWorker.perform_async(id)
-    Cache::PortfolioLiquidityFeesWorker.perform_async(id)
   end
 end
