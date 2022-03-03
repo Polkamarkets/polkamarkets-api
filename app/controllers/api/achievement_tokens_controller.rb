12ef7659ef5bcd4d6219774ce52933f08a880a8b
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-03-03 13:03:46 +0800

    achievement token controller tweaks

--- app/controllers/api/achievement_tokens_controller.rb
+++ app/controllers/api/achievement_tokens_controller.rb
@@ -4,10 +4,12 @@ module Api
       network_id = Rails.application.config_for(:networks)[params[:network].to_sym]
       raise "Network #{params[:network]} is not configured" if network_id.blank?
 
-      # finding items by eth id
-      token = AchievementToken.find_by!(eth_id: params[:id], network_id: network_id)
+      # checking if item was already created
+      token = AchievementToken.find_by(eth_id: params[:id], network_id: network_id)
+      # trying to create, if on blockchain (will raise an error if does not exist)
+      token ||= AchievementToken.create_from_eth_id!(network_id, params[:id])
 
-      render json: token, status: :ok
+      render json: token.reload, status: :ok
     end
   end
 end
