d57423a5de86670c1c4592878031f4c5dffbf8e6
Wellington Matheus <w.matheus@outlook.com>
2022-03-23 10:07:15 -0300

    refactor(NewsAPIService): use news-api gem

--- app/services/newsapi_service.rb
+++ app/services/newsapi_service.rb
@@ -1,20 +1,19 @@
+require 'news-api'
+require 'cgi'
+
 class NewsAPIService
-  VERSION = 'v2'.freeze
-  BASE_URL = "https://newsapi.org/#{VERSION}/".freeze
+  attr_accessor :service
 
   def initialize
-    @api_key = Rails.application.config_for(:newsapi).api_key
+    @service = News.new(Rails.application.config_for(:newsapi).api_key)
   end
 
-  def get_latest_news
-    [
-      {
-        title: "Canadian trucker protesters have bitcoin donations seized",
-        description: "The Canadian truckers protesting vaccine mandates had their donations seized, even if the contributions were made in Bitcoin, reported Vice's Motherboard on Tuesday. While the overwhelming majority (over 90 percent) of truckers in Canada have been vaccinated,…",
-        url: "https://www.rawstory.com/canadian-trucker-protest-bitcoin-taken/",
-        image_url: "https://www.rawstory.com/media-library/canada-police-ready-to-move-in-to-clear-trucker-led-protests.jpg?id=29368068&width=1200&coordinates=0%2C36%2C0%2C36&height=600",
-  
-      }
-    ]
+  def get_latest_news(category, subcategory)
+    latest_news = service.get_everything(q: CGI.escape("#{category} OR #{subcategory}"), 
+      language: 'en',
+      sortBy: 'relevancy'
+      pageSize: 3)
+    
+    latest_news.articles
   end
 end
