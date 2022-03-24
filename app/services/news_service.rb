9875849c985493f7b12ee3ff3ea622b3b33d55ab
Wellington Matheus <w.matheus@outlook.com>
2022-03-24 10:27:02 -0300

    refactor(Services): create News service

---
+++ app/services/news_service.rb
@@ -0,0 +1,27 @@
+require 'news-api'
+
+class NewsService
+  attr_accessor :service
+
+  def initialize
+    @service = News.new(Rails.application.config_for(:newsapi).api_key)
+  end
+
+  def get_latest_news(category, subcategory)
+    articles = service.get_everything(q: "#{category}%20AND%20#{subcategory}", 
+      language: 'en',
+      sortBy: 'relevancy',
+      pageSize: 3)
+    
+    latest_news = articles.map do |article|
+      {
+        title: article.title,
+        description: article.description,
+        url: article.url,
+        image_url: article.urlToImage
+      }
+    end
+
+    latest_news
+  end
+end
