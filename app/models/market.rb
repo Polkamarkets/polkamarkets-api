18d7c24e73a8bfd5ca97aea8c50a03f901519e17
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-04-07 11:04:57 +0800

    market: news aggregation cache tweaks

--- app/models/market.rb
+++ app/models/market.rb
@@ -215,21 +215,26 @@ class Market < ApplicationRecord
       .sum { |a| a[:value] }
   end
 
-  def keywords
-    title_keywords = TextRazorService.new.get_entities(title).sort_by { |e| e['confidenceScore'] }.reverse
-    title_keywords.select! do |entity|
-      entity['confidenceScore'] >= 1
+  def keywords(refresh: false)
+    Rails.cache.fetch("markets:network_#{network_id}:#{eth_market_id}:keywords", force: refresh) do
+      title_keywords = TextRazorService.new.get_entities(title).sort_by { |e| e['confidenceScore'] }.reverse
+      title_keywords.select! do |entity|
+        entity['confidenceScore'] >= 1
+      end
+
+      return [category, subcategory] if title_keywords.count == 0
+
+      title_keywords.map { |entity| entity['entityEnglishId'].presence || entity['matchedText'] }.uniq[0..2]
     end
-
-    return [category, subcategory] if title_keywords.count == 0
-
-    title_keywords.map { |entity| entity['entityEnglishId'].presence || entity['matchedText'] }.uniq[0..2]
   end
 
   def news(refresh: false)
     return [] if eth_market_id.blank?
 
-    Rails.cache.fetch("markets:network_#{network_id}:#{eth_market_id}:news", expires_in: 24.hours, force: refresh) do
+    Rails.cache.fetch("markets:network_#{network_id}:#{eth_market_id}:news", force: refresh) do
+      # only fetching news if market is not resolved
+      return [] if resolved?
+
       GnewsService.new.get_latest_news(keywords)
     end
   end
