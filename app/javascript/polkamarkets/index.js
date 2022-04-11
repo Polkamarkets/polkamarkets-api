ecf0ee0a0830511936f064c35c126a22f9b4216a
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-04-11 13:07:19 +0800

    remove unused javascript

--- app/javascript/polkamarkets/index.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap'
-
-document.addEventListener("turbolinks:load", function() {
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-        $('[data-toggle="popover"]').popover()
-    })
-})
