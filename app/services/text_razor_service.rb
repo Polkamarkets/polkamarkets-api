c9e65d39ee1dc44c7f14ce689c26aee1d8ff1dd7
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-04-12 14:08:13 +0800

    TextRazorService: add dictionaries

--- app/services/text_razor_service.rb
+++ app/services/text_razor_service.rb
@@ -4,7 +4,7 @@ class TextRazorService
 
     response = HTTP
       .headers(:'x-textrazor-key' => Rails.application.config_for(:text_razor).api_key)
-      .post(text_razor_url, form: { extractors: 'entities', text: text })
+      .post(text_razor_url, form: { extractors: 'entities', text: text, :'entities.dictionaries' => get_dictionaries })
 
     unless response.status.success?
       raise "TextRazorService #{response.status} :: #{response.body.to_s}"
@@ -13,6 +13,22 @@ class TextRazorService
     entities = JSON.parse(response.body.to_s)['response']
 
     entities['entities'] || []
+  end
+
+  def get_dictionaries(refresh: false)
+    return [] if Rails.application.config_for(:text_razor).api_key.blank?
+
+    Rails.cache.fetch("text_razor:dictionaries", force: refresh) do
+      response = HTTP
+        .headers(:'x-textrazor-key' => Rails.application.config_for(:text_razor).api_key)
+        .get(text_razor_url + 'entities/')
+
+      unless response.status.success?
+        raise "TextRazorService #{response.status} :: #{response.body.to_s}"
+      end
+
+      JSON.parse(response.body.to_s)['dictionaries'].map { |dictionary| dictionary['id'] }
+    end
   end
 
   private
