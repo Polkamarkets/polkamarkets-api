cd0623509bf2a16aa31b73b5f2e2f0f293c6bd6b
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-04-18 13:40:34 +0800

    StatsService: add daily stats

--- app/services/stats_service.rb
+++ app/services/stats_service.rb
@@ -95,6 +95,66 @@ class StatsService
     stats
   end
 
+  def get_stats_daily(from: nil, to: nil)
+    stats = networks.map do |network|
+      network_id = network[:network_id]
+      actions = network[:bepro_pm].get_action_events
+      market_ids = actions.map { |action| action[:market_id] }.uniq
+
+      create_market_actions = market_ids.map do |market_id|
+        # first action represents market creation
+        action = actions.find { |action| action[:market_id] == market_id }
+      end
+
+      # filtering by timestamps, if provided
+      actions.select! do |action|
+        (!from || action[:timestamp] >= from) &&
+          (!to || action[:timestamp] <= to)
+      end
+
+      create_market_actions.select! do |action|
+        (!from || action[:timestamp] >= from) &&
+          (!to || action[:timestamp] <= to)
+      end
+
+      # grouping actions by day intervals
+      actions_by_day = actions.group_by { |action| Time.at(action[:timestamp]).utc.beginning_of_day.to_i }
+
+      # fetching rate and fee values to avoid multiple API calls
+      rate = rate(network_id)
+      fee = network[:bepro_pm].get_fee
+
+      [
+        network_id,
+        actions_by_day.map do |timestamp, day_actions|
+          {
+            timestamp: timestamp,
+            markets_created: create_market_actions.select { |action| action[:timestamp].between?(timestamp, timestamp + 1.day.to_i) }.count,
+            volume: day_actions.select { |v| ['buy', 'sell'].include?(v[:action]) }.sum { |v| v[:value] },
+            volume_eur: day_actions.select { |v| ['buy', 'sell'].include?(v[:action]) }.sum { |v| v[:value] } * rate,
+            fees: day_actions.sum { |v| v[:value] } * fee,
+            fees_eur: day_actions.sum { |v| v[:value] } * fee * rate,
+            users: day_actions.map { |a| a[:address] }.uniq.count,
+            transactions: day_actions.count
+          }
+        end
+      ]
+    end.to_h
+
+    stats[:total] = stats.values.flatten.group_by { |v| v[:timestamp] }.map do |timestamp, day_stats|
+      {
+        timestamp: timestamp,
+        markets_created: day_stats.sum { |v| v[:markets_created] },
+        volume_eur: day_stats.sum { |v| v[:volume_eur] },
+        fees_eur: day_stats.sum { |v| v[:fees_eur] },
+        users: day_stats.sum { |v| v[:users] },
+        transactions: day_stats.sum { |v| v[:transactions] }
+      }
+    end
+
+    stats
+  end
+
   def rate(network_id)
     token = TokenRatesService::NETWORK_TOKENS[network_id.to_i]
 
