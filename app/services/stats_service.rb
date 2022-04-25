33143f0779f6209ffecefa31a0aece1074b9f454
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-04-25 22:15:59 +0800

    add category breakdown to stats service

--- app/services/stats_service.rb
+++ app/services/stats_service.rb
@@ -1,5 +1,12 @@
 class StatsService
-  attr_accessor :networks
+  attr_accessor :networks, :categories
+
+  TX_ACTIONS = [
+    'buy',
+    'sell',
+    'add_liquidity',
+    'remove_liquidity',
+  ].freeze
 
   def initialize
     # ethereum networks - markets are monitored within the api
@@ -96,9 +103,14 @@ class StatsService
   end
 
   def get_stats_daily(from: nil, to: nil)
-    stats = networks.map do |network|
+    stats = {}
+    all_actions = []
+
+    stats[:networks] = networks.to_h do |network|
       network_id = network[:network_id]
       actions = network[:bepro_pm].get_action_events
+      # used for category filtering
+      all_actions.concat(actions.map { |action| action.merge(network_id: network_id) })
       market_ids = actions.map { |action| action[:market_id] }.uniq
 
       create_market_actions = market_ids.map do |market_id|
@@ -127,27 +139,88 @@ class StatsService
       [
         network_id,
         actions_by_day.map do |timestamp, day_actions|
+          # summing actions values by tx_action
+          volume_by_tx_action = TX_ACTIONS.to_h do |action|
+            [
+              action,
+              day_actions.select { |a| a[:action] == action }.sum { |a| a[:value] }
+            ]
+          end
+
           {
             timestamp: timestamp,
             markets_created: create_market_actions.select { |action| action[:timestamp].between?(timestamp, timestamp + 1.day.to_i) }.count,
-            volume: day_actions.select { |v| ['buy', 'sell'].include?(v[:action]) }.sum { |v| v[:value] },
-            volume_eur: day_actions.select { |v| ['buy', 'sell'].include?(v[:action]) }.sum { |v| v[:value] } * rate,
-            tvl_volume: day_actions.select { |v| v[:action] == 'buy' }.sum { |v| v[:value] } - day_actions.select { |v| v[:action] == 'sell' }.sum { |v| v[:value] },
-            tvl_volume_eur: (day_actions.select { |v| v[:action] == 'buy' }.sum { |v| v[:value] } - day_actions.select { |v| v[:action] == 'sell' }.sum { |v| v[:value] }) * rate,
-            liquidity: day_actions.select { |v| ['add_liquidity', 'remove_liquidity'].include?(v[:action]) }.sum { |v| v[:value] },
-            liquidity_eur: day_actions.select { |v| ['add_liquidity', 'remove_liquidity'].include?(v[:action]) }.sum { |v| v[:value] } * rate,
-            tvl_liquidity: day_actions.select { |v| v[:action] == 'add_liquidity' }.sum { |v| v[:value] } - day_actions.select { |v| v[:action] == 'remove_liquidity' }.sum { |v| v[:value] },
-            tvl_liquidity_eur: (day_actions.select { |v| v[:action] == 'add_liquidity' }.sum { |v| v[:value] } - day_actions.select { |v| v[:action] == 'remove_liquidity' }.sum { |v| v[:value] }) * rate,
-            fees: day_actions.sum { |v| v[:value] } * fee,
-            fees_eur: day_actions.sum { |v| v[:value] } * fee * rate,
+            volume: volume_by_tx_action['buy'] + volume_by_tx_action['sell'],
+            volume_eur: (volume_by_tx_action['buy'] + volume_by_tx_action['sell']) * rate,
+            tvl_volume: volume_by_tx_action['buy'] - volume_by_tx_action['sell'],
+            tvl_volume_eur: (volume_by_tx_action['buy'] - volume_by_tx_action['sell']) * rate,
+            liquidity: volume_by_tx_action['add_liquidity'] + volume_by_tx_action['remove_liquidity'],
+            liquidity_eur: (volume_by_tx_action['add_liquidity'] + volume_by_tx_action['remove_liquidity']) * rate,
+            tvl_liquidity: volume_by_tx_action['add_liquidity'] - volume_by_tx_action['remove_liquidity'],
+            tvl_liquidity_eur: (volume_by_tx_action['add_liquidity'] - volume_by_tx_action['remove_liquidity']) * rate,
+            fees: (volume_by_tx_action['buy'] + volume_by_tx_action['sell']) * fee,
+            fees_eur: (volume_by_tx_action['buy'] + volume_by_tx_action['sell']) * fee * rate,
             users: day_actions.map { |a| a[:address] }.uniq.count,
             transactions: day_actions.count
           }
         end
       ]
-    end.to_h
+    end
 
-    stats[:total] = stats.values.flatten.group_by { |v| v[:timestamp] }.map do |timestamp, day_stats|
+    # grouping actions by market category
+    stats[:categories] = categories.to_h do |category|
+      network_ids = networks.map { |network| network[:network_id] }
+      # filtering action events by market category
+      actions = all_actions.select do |action|
+        markets_by_category[category].find { |market| market[:eth_market_id] == action[:market_id] && market[:network_id] == action[:network_id].to_i }
+      end
+      market_ids = actions.map { |action| action[:market_id] }.uniq
+
+      create_market_actions = market_ids.map do |market_id|
+        # first action represents market creation
+        action = actions.find { |action| action[:market_id] == market_id }
+      end
+
+      # filtering by timestamps, if provided
+      actions.select! do |action|
+        (!from || action[:timestamp] >= from) &&
+          (!to || action[:timestamp] <= to)
+      end
+
+      create_market_actions.select! do |action|
+        (!from || action[:timestamp] >= from) &&
+          (!to || action[:timestamp] <= to)
+      end
+
+      # grouping actions by day intervals
+      actions_by_day = actions.group_by { |action| Time.at(action[:timestamp]).utc.beginning_of_day.to_i }
+
+      [
+        category,
+        actions_by_day.map do |timestamp, day_actions|
+          # summing actions values by tx_action
+          volume_by_tx_action = TX_ACTIONS.to_h do |action|
+            [
+              action,
+              day_actions.select { |v| v[:action] == action }.group_by { |v| v[:network_id] }.map { |network_id, v| v.sum { |v| v[:value] * rate(network_id) } }.sum
+            ]
+          end
+
+          {
+            timestamp: timestamp,
+            markets_created: create_market_actions.select { |action| action[:timestamp].between?(timestamp, timestamp + 1.day.to_i) }.count,
+            volume_eur: volume_by_tx_action['buy'] + volume_by_tx_action['sell'],
+            tvl_volume_eur: volume_by_tx_action['buy'] - volume_by_tx_action['sell'],
+            liquidity_eur: volume_by_tx_action['add_liquidity'] + volume_by_tx_action['remove_liquidity'],
+            tvl_liquidity_eur: volume_by_tx_action['add_liquidity'] - volume_by_tx_action['remove_liquidity'],
+            users: day_actions.map { |a| a[:address] }.uniq.count,
+            transactions: day_actions.count
+          }
+        end
+      ]
+    end
+
+    stats[:total] = stats[:networks].values.flatten.group_by { |v| v[:timestamp] }.map do |timestamp, day_stats|
       {
         timestamp: timestamp,
         markets_created: day_stats.sum { |v| v[:markets_created] },
@@ -187,5 +260,13 @@ class StatsService
 
   def stats_network_ids
     @_stats_network_ids ||= Rails.application.config_for(:ethereum).stats_network_ids
+  end
+
+  def categories
+    @_categories ||= Market.pluck(:category).uniq
+  end
+
+  def markets_by_category
+    @_markets_by_category ||= Market.all.group_by(&:category)
   end
 end
