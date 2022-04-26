a8571317343516d20f55fa27451a5cd5883780bc
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-04-26 14:01:46 +0800

    stats service: move cache logic to service away from controller

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -14,14 +14,11 @@ module Api
     end
 
     def daily
-      if params[:from].blank? && params[:to].blank?
-        # only fetching cached stats without params
-        stats = Rails.cache.fetch("api:stats_daily", expires_in: 24.hours) do
-          StatsService.new.get_stats_daily
-        end
-      else
-        stats = StatsService.new.get_stats_daily(from: params[:from].to_i, to: params[:to].to_i)
-      end
+      stats = StatsService.new.get_stats_by_timeframe(
+        from: params[:from],
+        to: params[:to],
+        timeframe: params[:timeframe] || '1d'
+      )
 
       render json: stats, status: :ok
     end
