4ce174d44db6ace9248042623070b9b1c76a213a
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-07-13 11:52:31 +0100

    only perform market update banner image in production

--- app/workers/market_banner_worker.rb
+++ app/workers/market_banner_worker.rb
@@ -2,6 +2,9 @@ class MarketBannerWorker
   include Sidekiq::Worker
 
   def perform(market_id)
+    # only performing in production
+    return unless Rails.env.production?
+
     market = Market.find(market_id)
     return if market.blank?
 
