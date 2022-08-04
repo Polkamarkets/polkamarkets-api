ce04d06d2e01208b53936d26bd78d35b6cf0524b
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-08-04 12:23:48 +0200

    feed_service: add create_market actions to portfolio feed

--- app/services/feed_service.rb
+++ app/services/feed_service.rb
@@ -20,6 +20,10 @@ class FeedService
       .sort_by { |a| -a[:timestamp] }
   end
 
+  def create_event_actions
+    @create_event_actions ||= Bepro::PredictionMarketContractService.new(network_id: network_id).get_events(event_name: 'MarketCreated')
+  end
+
   def markets
     @markets ||= Market.where(network_id: network_id, eth_market_id: actions.map { |a| a[:market_id] }.uniq).includes(:outcomes)
   end
@@ -36,6 +40,10 @@ class FeedService
     filtered_actions.map do |action|
       market = markets.find { |m| m.eth_market_id == action[:market_id] }
       outcome = market.outcomes.find { |o| o.eth_market_id == action[:outcome_id] } if action[:action] == 'buy' || action[:action] == 'sell'
+      # for optimization purposes, determining if add_liquidity action is a create_market action by timestamp proximity
+      if action[:action] == 'add_liquidity' && create_event_actions.any? { |e| e['transactionHash'] == action[:tx_id] }
+        action[:action] = 'create_market'
+      end
 
       {
         user: action[:address],
