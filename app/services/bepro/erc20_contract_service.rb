54bbcde05465c7c356a4f715875749b1c77d6dd6
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-11-22 16:58:47 +0000

    add erc20 contract service

---
+++ app/services/bepro/erc20_contract_service.rb
@@ -0,0 +1,23 @@
+module Bepro
+  class Erc20ContractService < SmartContractService
+    include BigNumberHelper
+
+    def initialize(network_id: nil, api_url: nil, contract_address: nil)
+      super(
+        contract_name: 'erc20',
+        contract_address:
+          contract_address ||
+            Rails.application.config_for(:ethereum).dig(:"network_#{network_id}", :erc20_contract_address) ||
+            Rails.application.config_for(:ethereum).dig(:"stats_network_#{network_id}", :erc20_contract_address),
+        api_url:
+          api_url ||
+            Rails.application.config_for(:ethereum).dig(:"network_#{network_id}", :bepro_api_url) ||
+            Rails.application.config_for(:ethereum).dig(:"stats_network_#{network_id}", :bepro_api_url),
+      )
+    end
+
+    def balance_of(user)
+      from_big_number_to_float(call(method: 'balanceOf', args: user))
+    end
+  end
+end
