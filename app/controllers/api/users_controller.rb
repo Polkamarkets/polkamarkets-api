22e56a4a051c3e16754340976fe148c03ec7482e
Andre Goncalves <andrefcgoncalves@gmail.com>
2023-05-29 19:16:14 +0100

    Save discord servers and avatar

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,11 +6,15 @@ module Api
       username = params[:username]
       wallet_address = params[:wallet_address]
       login_type = params[:login_type]
+      discord_servers = params[:discord_servers]
+      avatar = params[:avatar]
 
       current_user.update(
         username: username,
         wallet_address: wallet_address,
-        login_type: login_type
+        login_type: login_type,
+        discord_servers: discord_servers,
+        avatar: avatar
       )
 
       render json: { update: 'ok' }, status: :ok
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_11_183224) do
+ActiveRecord::Schema.define(version: 2023_05_23_182601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,8 @@ ActiveRecord::Schema.define(version: 2023_05_11_183224) do
     t.string "login_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.jsonb "discord_servers"
+    t.string "avatar"
   end
 
   add_foreign_key "achievement_tokens", "achievements"
