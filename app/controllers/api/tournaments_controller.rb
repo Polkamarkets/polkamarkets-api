91da8ac8b8c1efe3494415445e45ee33d0b5db13
Ricardo Marques <ricardo.ds.marques@gmail.com>
2023-06-23 12:54:18 -0600

    fix(tournaments): create/update params

--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -13,7 +13,15 @@ module Api
     end
 
     def create
-      tournament = Tournament.new(tournament_params)
+      create_params = tournament_params.except(:market_ids)
+
+      # converting market_ids to markets
+      create_params[:market_ids] = tournament_params[:market_ids].map do |market_id|
+        market = Market.find_by!(eth_market_id: market_id, network_id: tournament_params[:network_id])
+        market.id
+      end
+
+      tournament = Tournament.new(create_params)
 
       if tournament.save
         render json: tournament, status: :created
@@ -25,7 +33,15 @@ module Api
     def update
       tournament = Tournament.friendly.find(params[:id])
 
-      if tournament.update(tournament_params)
+      update_params = tournament_params.except(:market_ids)
+
+      # converting market_ids to markets
+      update_params[:market_ids] = tournament_params[:market_ids].map do |market_id|
+        market = Market.find_by!(eth_market_id: market_id, network_id: tournament_params[:network_id])
+        market.id
+      end
+
+      if tournament.update(update_params)
         render json: tournament
       else
         render json: tournament.errors, status: :unprocessable_entity
@@ -41,7 +57,7 @@ module Api
     private
 
     def tournament_params
-      params.require(:tournament).permit(:title, :description, :image_url, :network_id, market_ids: [])
+      params.require(:tournament).permit(:id, :title, :description, :image_url, :network_id, market_ids: [])
     end
   end
 end
