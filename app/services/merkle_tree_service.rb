ee6a9be862d73f2785fbd065ed9943cf413c3bff
Andre Goncalves <andrefcgoncalves@gmail.com>
2023-09-27 20:21:04 +0100

    Call merkle tree api

--- app/services/merkle_tree_service.rb
+++ app/services/merkle_tree_service.rb
@@ -67,12 +67,10 @@ class MerkleTreeService
           # call new api to get merkle tree with proof
           merkle_tree = call_merkle_tree_api(network_id, converted_amounts)
 
-          # FIXME check if next line is needed
-          # merkle_tree = merkle_tree.parse(:json)
 
           # Convert merkle tree amounts from big number to integer
-          merkle_tree[:claims].each do |user_address, merkle_data|
-            merkle_data[:amount] = from_big_number_to_integer(merkle_data[:amount], decimals)
+          merkle_tree['claims'].each do |user_address, merkle_data|
+            merkle_data['amount'] = from_big_number_to_integer(merkle_data['amount'], decimals)
           end
 
           # save on the database
@@ -90,13 +88,11 @@ class MerkleTreeService
   private
 
   def call_merkle_tree_api(network_id, converted_amounts)
-    # FIXME call service
-    # HTTP.post(
-    #   "#{Rails.application.config_for(:ethereum)[:"rewards_network_#{network_id}"][:bepro_api_url]}/merkle-tree",
-    #   json: {
-    #     amounts: converted_amounts
-    #   }
-    # )
-    {"merkleRoot":"0x05e857ad2627320294fdc1904372ccfbdae1f892dbfc6f3292d7647001d2306b","claims":{"0x005DCB2C539EB682F14B7441863C298904D108BD":{"index":0,"amount":2e+21,"proof":["0xca000eeb0e421d2fa0b82d0557493d4b5e0f05310750c2a86f3976d85fccbe70"]},"0x6122252DC9BE4DBF4DF78C22E5348A12B1C77D61":{"index":1,"amount":5e+21,"proof":["0x8ffa2c85501a8c34ac1e84f1ffbdcdba19f4967fa414cd8b4cd13b1598ff50d5","0xcf793acd9b963171c2d69ac885b9fe144adf456f00936a23153df253d3620325"]},"0xEF4D8DC13FDEB0F2B4784B1DAE743093C228A08A":{"index":2,"amount":5e+21,"proof":["0x10b51283d1db97a125b2c4cf0a2b8a7475e5001ff53cf98871ab174cdc26baea","0xcf793acd9b963171c2d69ac885b9fe144adf456f00936a23153df253d3620325"]}}}
+    response = HTTP.post(
+      "#{Rails.application.config_for(:ethereum)[:merkle_generator_api_url]}",
+      json: converted_amounts
+    )
+
+    JSON.parse(response)
   end
 end
