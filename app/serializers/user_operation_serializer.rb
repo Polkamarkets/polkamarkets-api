f828f22a12ee1be14629680534680034c7176d1d
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-01-27 18:46:59 +0000

    user_operation serializer add network_id, market_id, outcome_id

--- app/serializers/user_operation_serializer.rb
+++ app/serializers/user_operation_serializer.rb
@@ -1,12 +1,15 @@
 class UserOperationSerializer < ActiveModel::Serializer
   attributes(
+    :network_id,
     :user,
     :user_operation_hash,
     :status,
     :action,
     :transaction_hash,
+    :market_id,
     :market_title,
     :market_slug,
+    :outcome_id,
     :outcome_title,
     :image_url,
     :shares,
@@ -17,5 +20,13 @@ class UserOperationSerializer < ActiveModel::Serializer
 
   def user
     object.user_address
+  end
+
+  def market_id
+    object.market&.id
+  end
+
+  def outcome_id
+    object.outcome&.id
   end
 end
