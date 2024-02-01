e8aad2545add6ff95af01fbe154851f87ff59698
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-02-01 00:05:14 +0000

    add retries to user_operation workers

--- app/workers/user_operation/send_to_bundler_worker.rb
+++ app/workers/user_operation/send_to_bundler_worker.rb
@@ -5,7 +5,17 @@ class UserOperation::SendToBundlerWorker
     user_operation = UserOperation.find_by(id: user_operation_id)
     return if user_operation.blank?
 
-    response = BundlerService.new.process_user_operation(user_operation.user_operation, user_operation.network_id)
+    response = nil
+
+    # trying 3 times to send the operation with 1 second intervals
+    tries = 3
+    tries.times do
+      response = BundlerService.new.process_user_operation(user_operation.user_operation, user_operation.network_id)
+      break if response.dig('error').blank?
+
+      user_operation.update(retries: user_operation.retries + 1)
+      sleep 1
+    end
 
     if response.dig('error').present?
       user_operation.update(status: :failed, error_message: "#{response.dig('error', 'code')}: #{response.dig('error', 'message')}")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_27_221516) do
+ActiveRecord::Schema.define(version: 2024_01_31_234933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(version: 2024_01_27_221516) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "error_message"
+    t.integer "retries", default: 0
     t.index ["network_id", "user_address"], name: "index_user_operations_on_network_id_and_user_address"
     t.index ["user_address"], name: "index_user_operations_on_user_address"
     t.index ["user_operation_hash"], name: "index_user_operations_on_user_operation_hash", unique: true
