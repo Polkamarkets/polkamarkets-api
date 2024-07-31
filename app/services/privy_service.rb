dc43c8b605780f19d16c62a405ec2f7b30c65565
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-07-31 18:54:08 +0100

    feat(users): add UserIdpsWorker

--- app/services/privy_service.rb
+++ app/services/privy_service.rb
@@ -9,36 +9,29 @@ class PrivyService
   end
 
   def get_user_data(user_id:)
-    uri = api_url + "/api/v1/users/#{user_id}"
+    uri = privy_url + "/users/#{user_id}"
 
-    Sentry.with_scope do |scope|
-      scope.set_tags(
-        uri: uri
-      )
+    response = HTTP.basic_auth(user: app_id, pass: app_secret)
+      .headers('privy-app-id' => app_id)
+      .get(uri)
+    response_body = response.body.to_s
 
-      begin
-        response = HTTP.basic_auth(user: app_id, pass: app_secret)
-              .headers('privy-app-id' => app_id)
-              .get(uri)
-        response_body = response.body.to_s
+    raise "PrivyService :: Get User Data Error" unless response.status.success?
 
-        unless response.status.success? && !response_body.include?('server unavailable')
-          puts response_body.to_s
-          scope.set_tags(
-            status: response.status,
-            error: response_body.to_s
-          )
-          raise "PrivyService :: Get User Data Error"
-        end
+    parse_data(JSON.parse(response_body.to_s))
+  end
 
-        parse_data(JSON.parse(response_body.to_s))
-      rescue => e
-        scope.set_tags(
-          error: e.message
-        )
-        raise e.message
-      end
-    end
+  def search_users(query: {})
+    uri = privy_url + "/users/search"
+
+    response = HTTP.basic_auth(user: app_id, pass: app_secret)
+      .headers('privy-app-id' => app_id)
+      .headers('Content-Type' => 'application/json')
+      .post(uri, body: query.to_json)
+
+    raise "PrivyService :: Search Users Error" unless response.status.success?
+
+    JSON.parse(response.body.to_s)
   end
 
   def uid_from_linked_account_data(linked_account)
@@ -76,4 +69,7 @@ class PrivyService
     }
   end
 
+  def privy_url
+    "https://auth.privy.io/api/v1"
+  end
 end
---
+++ app/workers/user_idps_worker.rb
@@ -0,0 +1,26 @@
+class UserIdpsWorker
+  include Sidekiq::Worker
+
+  def perform(user_id)
+    user = User.find_by(id: user_id)
+    return if user.blank? || user.login_public_key.blank?
+
+    privy_service = PrivyService.new
+
+    # finding privy user by embedded wallet address
+    users_data = privy_service.search_users(query: { walletAddresses: [user.login_public_key] })
+
+    return if users_data['data'].blank?
+
+    user_data = users_data['data'].first
+
+    # creating user_idp for the user
+    user.update(idp: 'privy', idp_uid: user_data['id'])
+    user_data['linked_accounts'].each do |linked_account|
+      idp_uid = privy_service.uid_from_linked_account_data(linked_account)
+      user_idp = user.user_idps.find_or_initialize_by(provider: linked_account['type'], uid: idp_uid)
+      user_idp.data = linked_account
+      user_idp.save!
+    end
+  end
+end
