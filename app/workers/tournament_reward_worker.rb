6e92f4079551aa87336185596140c6f47cff8aca
Andre Goncalves <andrefcgoncalves@gmail.com>
2024-10-24 07:56:35 +0100

    Refactor to use tournament rake

--- app/workers/tournament_reward_worker.rb
+++ app/workers/tournament_reward_worker.rb
@@ -1,26 +1,26 @@
 class TournamentRewardWorker
   include Sidekiq::Worker
 
-  def perform()
+  def perform(tournament_id)
 
     # Find tournaments that have not been computed
-    tournaments = Tournament.where(computed_claims: false)
+    tournament = Tournament.find_by(id: tournament_id)
 
-    tournaments.each do |tournament|
-      # if tournament is not resolved, skip
-      next unless tournament.resolved?
+    return if tournament.blank? || tournament.computed_claims?
 
-      # compute leaderboard
-      leaderboard = LeaderboardService.new.get_tournament_leaderboard(tournament.network_id, tournament.id, refresh: true)
+    # if tournament is not resolved, return
+    return unless tournament.resolved?
 
-      leaderboard.each_with_index do |user_data, index|
-        rank = index + 1
-        reward = tournament.rewards.find { |reward| rank >= reward['from'] && rank <= reward['to'] }
+    # compute leaderboard
+    leaderboard = LeaderboardService.new.get_tournament_leaderboard(tournament.network_id, tournament.id, refresh: true)
 
-        ClaimService.new.create_claim(tournament.network_id, user_data[:user], reward['value'], 'tournament', { tournament_id: tournament.id })
-      end
+    leaderboard.each_with_index do |user_data, index|
+      rank = index + 1
+      reward = tournament.rewards.find { |reward| rank >= reward['from'] && rank <= reward['to'] }
 
-      tournament.update!(computed_claims: true)
+      ClaimService.new.create_claim(tournament.network_id, user_data[:user], reward['value'], 'tournament', { tournament_id: tournament.id })
     end
+
+    tournament.update!(computed_claims: true)
   end
 end
