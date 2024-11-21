1bacff91905bad16a6fdded3beb8c22caf017061
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-11-21 11:23:54 +0000

    feat(og_images): tweaks

--- app/models/concerns/og_imageable.rb
+++ app/models/concerns/og_imageable.rb
@@ -2,14 +2,16 @@ module OgImageable
   extend ActiveSupport::Concern
 
   included do
-    after_create :generate_og_image
+    after_create :update_og_image_async
 
-    def generate_og_image
+    def update_og_image_async
       OgImageWorker.perform_async(self.class.name, id)
     end
 
-    def upload_og_image(file_path)
+    def update_og_image
       begin
+        file_path = ScreenshotService.new.capture(og_image_web_url)
+
         file = File.open(file_path)
 
         cloudflare_service = CloudflareService.new
@@ -18,16 +20,20 @@ module OgImageable
         variant = response['result']['variants'].find { |v| v.include?('public') }
         raise "No public variant found for OG image" if variant.blank?
 
+        current_og_image_url = og_image_url
+
         self.og_image_url = variant
         self.save!
+
+        # delete previous image for storage optimization
+        cloudflare_service.delete_image_from_url(current_og_image_url) if current_og_image_url.present?
       ensure
         File.delete(file_path) if File.exist?(file_path)
-
       end
     end
 
-    def og_image_url_before_upload
-      "#{ENV['OG_IMAGES_URL']}/og/#{self.class::OG_IMAGEABLE_PATH}/#{slug}"
+    def og_image_web_url
+      "#{Rails.application.config_for(:polkamarkets).web_url}/og/#{self.class::OG_IMAGEABLE_PATH}/#{slug}"
     end
   end
 end
--- app/workers/og_image_worker.rb
+++ app/workers/og_image_worker.rb
@@ -2,10 +2,9 @@ class OgImageWorker
   include Sidekiq::Worker
 
   def perform(resource_type, resource_id)
-    resource = resource_type.constantize.find(resource_id)
+    resource = resource_type.constantize.find_by(id: resource_id)
+    return if resource.blank?
 
-    screenshot_path = ScreenshotService.new.capture(resource.og_image_url_before_upload)
-
-    resource.upload_og_image(screenshot_path)
+    resource.update_og_image
   end
 end
