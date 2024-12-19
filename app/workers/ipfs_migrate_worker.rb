bcf8f0f6e7383cd96c4ddfeabaa7c28f360e34ee
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-12-19 19:41:09 +0000

    fix(ipfs_migrate): drop job if model not found

--- app/workers/ipfs_migrate_worker.rb
+++ app/workers/ipfs_migrate_worker.rb
@@ -2,7 +2,9 @@ class IpfsMigrateWorker
   include Sidekiq::Worker
 
   def perform(model_name, model_id, field_name)
-    model = model_name.constantize.find(model_id)
+    model = model_name.constantize.find_by(id: model_id)
+    return if model.blank?
+
     model.migrate_ipfs_image_to_cloudflare(field_name)
   end
 end
