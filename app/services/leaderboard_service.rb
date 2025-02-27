67a544d4b4ae333aeca3d8ec7bdaf44d1bfe4137
Ricardo Marques <ricardo.ds.marques@gmail.com>
2025-02-27 11:40:41 +0000

    feat(leaderboard): store rankings in redis

--- app/services/leaderboard_service.rb
+++ app/services/leaderboard_service.rb
@@ -335,13 +335,15 @@ class LeaderboardService
   end
 
   def write_leaderboard_to_redis(leaderboard, cache_key)
+    return if leaderboard.blank?
+
     # ranking by earnings
     earnings = leaderboard.map { |l| [l[:earnings_eur], l[:user]] }
-    $redis_store.zadd("#{cache_key}:earnings", earnings) if earnings.present?
+    $redis_store.zadd("#{cache_key}:earnings", earnings)
 
     # ranking by won predictions
     won_predictions = leaderboard.map { |l| [l[:claim_winnings_count], l[:user]] }
-    $redis_store.zadd("#{cache_key}:won_predictions", won_predictions) if won_predictions.present?
+    $redis_store.zadd("#{cache_key}:won_predictions", won_predictions)
 
     # writing leaderboard to set
     data = leaderboard.map { |l| ["#{cache_key}:data:#{l[:user]}", map_leaderboard_entry_to_redis(l)] }.flatten
