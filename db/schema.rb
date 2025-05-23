28a2b4de02f98674099d5334b14d127292f9dbe0
Ricardo Marques <ricardo.ds.marques@gmail.com>
2025-05-23 02:38:11 +0100

    feat(markets): add publish_market_enabled flag to scheduler

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_05_05_163655) do
+ActiveRecord::Schema.define(version: 2025_05_23_013557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,6 +196,7 @@ ActiveRecord::Schema.define(version: 2025_05_05_163655) do
     t.boolean "active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "publish_market_enabled", default: false
     t.index ["market_template_id"], name: "index_market_schedules_on_market_template_id"
   end
 
